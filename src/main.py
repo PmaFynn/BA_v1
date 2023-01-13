1cdc622c36fb73e5cd1ff6a29bec08d83c9a3291
PmaFynn <fynn120801@gmail.com>
2023-01-13 17:10:07 +0100

    pytset being done on dev/production not locally

--- src/main.py
+++ src/main.py
@@ -36,8 +36,8 @@ baseAboutPage = './compImages/AboutRef.png'
 baseHomePage = './compImages/HomeRef.png'
 #driver = webdriver.Chrome()
 #driver.set_window_size(1400, 1050)
-url = "http://localhost:3000"
-#url = "https://www.google.com/"
+#url = "http://localhost:3000" local
+url = "https://pmafynn.github.io/BA_v1/"
 driver.get(url) #local als auch 'on your network' funktioniert beides
 
 #PATH = "C:\Devlopment\SeleniumDrivers\chromedriver.exe"
