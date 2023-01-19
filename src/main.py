28d70f41d463608d8dcb7d957af2e65e751d90a4
PmaFynn <fynn120801@gmail.com>
2023-01-19 18:14:43 +0100

    Perhaps the greatest commit in the history of commits

--- src/main.py
+++ src/main.py
@@ -157,7 +157,25 @@ def main(width, height):
         print("Visual Regression Test for", width, height, "failed. Reverting to last commit suggested to fix it")
         return False
 
+def takeImagesForPush():
+    driver.set_window_size(1400, 1050)
+    screenshot(1400, 1050, 'Home', "Home")
+    screenshot(1400, 1050, 'Pricing', "Pricing")
+    screenshot(1400, 1050, 'About', "About")
+    driver.set_window_size(1920, 1080)
+    screenshot(1920, 1080, 'Home', "Home")
+    screenshot(1920, 1080, 'Pricing', "Pricing")
+    screenshot(1920, 1080, 'About', "About")
+    driver.set_window_size(828, 1792)
+    screenshot(828, 1792, 'Home', "Home")
+    screenshot(828, 1792, 'Pricing', "Pricing")
+    screenshot(828, 1792, 'About', "About")
+    driver.set_window_size(1280, 800)
+    screenshot(1280, 800, 'Home', "Home")
+    screenshot(1280, 800, 'Pricing', "Pricing")
+    screenshot(1280, 800, 'About', "About")
 
+takeImagesForPush()
 
 #takeNewRefImages()
 #RefForAllSizes()
