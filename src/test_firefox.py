2546d74b81120d01323aac77370df2b428e37fae
PmaFynn <fynn120801@gmail.com>
2023-03-14 17:39:57 +0100

    Showcase for bachelor thesis - Visual regression test should fail

--- src/test_firefox.py
+++ src/test_firefox.py
@@ -1,13 +1,13 @@
 from mainFirefox import mainFirefox
 
-def test_1400x1050():
+def test_firefox_1400x1050():
         assert mainFirefox(1400, 1050) == True
 
-def test_1920x1080():
+def test_firefox_1920x1080():
         assert mainFirefox(1920, 1080) == True
 
-def test_828x1792():
+def test_firefox_828x1792():
         assert mainFirefox(828, 1792) == True
 
-def test_1280x800():
+def test_firefox_1280x800():
         assert mainFirefox(1280, 800) == True
